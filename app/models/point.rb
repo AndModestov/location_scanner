8450233af60874c843b06df78d78e2e7bcd2de00
AndModestov <and.modestov@gmail.com>
2017-03-05 13:39:23 +0300

    add calculate_range methods for point

--- app/models/point.rb
+++ app/models/point.rb
@@ -2,4 +2,23 @@ class Point < ActiveRecord::Base
   validates :latitude, :longitude, presence: true
   validates_inclusion_of :latitude, in: -90..90
   validates_inclusion_of :longitude, in: -180..180
+
+  EARTH_R = 6371
+  PI = 3.141592
+
+  def calculate_range target_point_id
+    target_point = Point.find(target_point_id)
+    lat1 = to_radians(latitude)
+    lon1 = to_radians(longitude)
+    lat2 = to_radians(target_point.latitude)
+    lon2 = to_radians(target_point.longitude)
+
+    sin1 = Math.sin((lat1-lat2)/2)
+    sin2 = Math.sin((lon1-lon2)/2)
+    range = EARTH_R*2*Math.asin(Math.sqrt(sin1*sin1+sin2*sin2*Math.cos(lat1)*Math.cos(lat2)))
+  end
+
+  def to_radians deg
+    deg*PI/180
+  end
 end
--- spec/models/point_spec.rb
+++ spec/models/point_spec.rb
@@ -5,4 +5,21 @@ RSpec.describe Point, type: :model do
   it { should validate_presence_of :longitude }
   it { should validate_inclusion_of(:latitude).in_range(-90..90) }
   it { should validate_inclusion_of(:longitude).in_range(-180..180) }
+
+  describe 'to_radians method' do
+    let!(:point){ create(:point) }
+
+    it 'converts degrees to radians' do
+      expect(point.to_radians 45).to eq 0.785398
+    end
+  end
+
+  describe 'calculate_range method' do
+    let!(:point1){ create(:point, latitude: 55.75, longitude: 37.616) }
+    let!(:point2){ create(:point, latitude: 59.8833, longitude: 30.25) }
+
+    it 'converts degrees to radians' do
+      expect(point1.calculate_range point2.id).to eq 632.9762529835119
+    end
+  end
 end
